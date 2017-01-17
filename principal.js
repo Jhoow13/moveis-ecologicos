b751d789ecd7d0af6177ceb313e23c9c27b4ffc6
Joel Silva <joelcsilva@outlook.com>
2017-01-17 00:33:01 -0200

    Inserindo arquivo principal.js

---
+++ principal.js
@@ -0,0 +1,9 @@
+var banners = ["Os melhores do Brasil", "Qualidade e preço baixo!"];
+var bannerAtual = 0;
+
+function trocaBanner(){
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 2000)
