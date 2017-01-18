4b5e02eea9671a3c04ead73dc94511cc64a92be9
Joel Silva <joelcsilva@outlook.com>
2017-01-17 22:42:31 -0200

    Alterando textos do banner

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,4 @@
-var banners = ["Os melhores do Brasil", "Qualidade e preço baixo!"];
+var banners = ["Do lixo ao luxo", "Reaproveitar é aproveitar!"];
 var bannerAtual = 0;
 
 function trocaBanner(){
